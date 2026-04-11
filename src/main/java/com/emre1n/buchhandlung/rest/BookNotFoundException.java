5f81fb62ceb506b2bbdb15ba4ed5865ff63688ea
emre1n <emre1n@protonmail.com>
2026-04-11 23:30:17 +0300

    add custom exception and exception handling

---
+++ src/main/java/com/emre1n/buchhandlung/rest/BookNotFoundException.java
@@ -0,0 +1,16 @@
+package com.emre1n.buchhandlung.rest;
+
+public class BookNotFoundException extends RuntimeException {
+
+    public BookNotFoundException(String message) {
+        super(message);
+    }
+
+    public BookNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BookNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
--- src/main/java/com/emre1n/buchhandlung/rest/BookRestController.java
+++ src/main/java/com/emre1n/buchhandlung/rest/BookRestController.java
@@ -2,10 +2,11 @@ package com.emre1n.buchhandlung.rest;
 
 import com.emre1n.buchhandlung.entity.Book;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -14,6 +15,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class BookRestController {
+
+    private static final Logger log = LoggerFactory.getLogger(BookRestController.class);
 
     private List<Book> books;
 
@@ -46,9 +49,49 @@ public class BookRestController {
     }
 
     @GetMapping("/books/{bookId}")
-    public Book getBook(@PathVariable("bookId") int bookId) {
+    public Book getBook(@PathVariable int bookId) {
         // to keep it simple for now just index into the list
 
+        // check the bookId against the list size
+
+        if (bookId >= books.size() || bookId < 0) {
+            throw new BookNotFoundException("Book id not found - "  + bookId);
+        }
+
         return books.get(bookId);
+    }
+
+    // Add an exception handler using @ExceptionHandler
+    @ExceptionHandler
+    public ResponseEntity<BookErrorResponse> handleException (BookNotFoundException exc) {
+
+        // create a BookErrorResponse
+
+        BookErrorResponse error = new BookErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+        //return ResponseEntity
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    // add another exception handler ... to catch any exception (catch all)
+
+    @ExceptionHandler
+    public ResponseEntity<BookErrorResponse> handleException(Exception exc) {
+
+        // create a BookErrorResponse
+
+        BookErrorResponse error = new BookErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(
+                "Please check your input and try again.");
+        error.setTimeStamp(System.currentTimeMillis());
+        //return ResponseEntity
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
